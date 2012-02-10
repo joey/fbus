ec0c4a656eccb729ad95e907333fe0583db857ed
Joseph Echeverria <joey@cloudera.com>
2012-02-10 15:47:18 -0500

    Fixed formatting, no functional changes.

--- src/main/java/com/cloudera/fbus/HDFSSequenceFileDirectoryDestination.java
+++ src/main/java/com/cloudera/fbus/HDFSSequenceFileDirectoryDestination.java
@@ -27,16 +27,14 @@ import org.springframework.util.Assert;
 public class HDFSSequenceFileDirectoryDestination {
 
   private static final String tmpSuffix = ".tmp";
-  private static final Logger logger = LoggerFactory
-      .getLogger(HDFSDirectoryDestination.class);
-
+  private static final Logger logger = LoggerFactory.getLogger(HDFSDirectoryDestination.class);
   private File directory;
   private FileSystem fileSystem;
   private CompressionCodec compressionCodec = new SnappyCodec(); //Snappy is the default compression
   private Configuration config;
 
   public HDFSSequenceFileDirectoryDestination() throws IOException {
-	config = new Configuration();
+    config = new Configuration();
     fileSystem = FileSystem.get(config);
   }
 
@@ -54,15 +52,14 @@ public class HDFSSequenceFileDirectoryDestination {
     destination = new Path(directory.getPath(), source.getName());
 
     try {
-      
+
       copyFromLocalToSeqFile(file, destinationTmp);
 
       if (!fileSystem.rename(destinationTmp, destination)) {
         if (!fileSystem.delete(destinationTmp, false)) {
-          logger
-              .warn(
-                  "Failed to clean up temporary file at {} after rename failed. File will be retried.",
-                  destinationTmp);
+          logger.warn(
+              "Failed to clean up temporary file at {} after rename failed. File will be retried.",
+              destinationTmp);
         }
 
         throw new IOException("Can't rename " + destinationTmp + " to "
@@ -73,10 +70,9 @@ public class HDFSSequenceFileDirectoryDestination {
            * Purposefully do not throw an exception here as it would cause retry
            * as well. -esammer
            */
-          logger
-              .error(
-                  "Unable to delete file {} after moving it to HDFS @ {}. File may be retransfered!",
-                  file, destination);
+          logger.error(
+              "Unable to delete file {} after moving it to HDFS @ {}. File may be retransfered!",
+              file, destination);
         }
       }
     } catch (Throwable e) {
@@ -86,30 +82,27 @@ public class HDFSSequenceFileDirectoryDestination {
   }
 
   private void copyFromLocalToSeqFile(File file, Path destinationTmp) throws IOException, FileNotFoundException {
-	SequenceFile.Writer writer = null;
-	BufferedReader reader = null;
-	  
-	  
-	try{
-	  writer = SequenceFile.createWriter(fileSystem, config, destinationTmp, NullWritable.class, Text.class, SequenceFile.CompressionType.BLOCK, compressionCodec);
-	  reader = new BufferedReader(new FileReader(file));
-	  
-	  String line = null;
-	  Text text = new Text();
-	  while((line = reader.readLine()) != null){
-	    text.set(line);
-	    writer.append(NullWritable.get(), text);	
-	}
-	}finally
-	{   
-	  if (writer != null)
-	  {
-	    writer.close();
-	  }
-	  if (reader != null)
-	  {
-  		reader.close();
-	  }
+    SequenceFile.Writer writer = null;
+    BufferedReader reader = null;
+
+
+    try {
+      writer = SequenceFile.createWriter(fileSystem, config, destinationTmp, NullWritable.class, Text.class, SequenceFile.CompressionType.BLOCK, compressionCodec);
+      reader = new BufferedReader(new FileReader(file));
+
+      String line = null;
+      Text text = new Text();
+      while ((line = reader.readLine()) != null) {
+        text.set(line);
+        writer.append(NullWritable.get(), text);
+      }
+    } finally {
+      if (writer != null) {
+        writer.close();
+      }
+      if (reader != null) {
+        reader.close();
+      }
     }
   }
 
@@ -120,15 +113,12 @@ public class HDFSSequenceFileDirectoryDestination {
   public void setDirectory(File directory) {
     this.directory = directory;
   }
-  
-  public CompressionCodec getCompressionCodec()
-  {
-	  return compressionCodec;
-  }
-  
-  public void setCompressionCodec(CompressionCodec compressionCodec)
-  {
-	  this.compressionCodec = compressionCodec; 
+
+  public CompressionCodec getCompressionCodec() {
+    return compressionCodec;
   }
 
+  public void setCompressionCodec(CompressionCodec compressionCodec) {
+    this.compressionCodec = compressionCodec;
+  }
 }
