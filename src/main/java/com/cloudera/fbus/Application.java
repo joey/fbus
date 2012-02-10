3270f9fad59b952872b7e773a11f61ad9c7083dc
Joseph Echeverria <joey@cloudera.com>
2012-02-10 15:39:47 -0500

    Updated the assembly to build a tarball with jars in a lib directory. Added an fbus-default.xml to pre-define compression codecs.

--- src/main/java/com/cloudera/fbus/Application.java
+++ src/main/java/com/cloudera/fbus/Application.java
@@ -16,6 +16,7 @@ public class Application {
 
   private static final Logger logger = LoggerFactory
       .getLogger(Application.class);
+  private static final String defaultConfigLocation = "classpath:fbus-default.xml";
   private static final String configLocation = "classpath:fbus.xml";
 
   public static void main(String[] args) {
@@ -34,7 +35,7 @@ public class Application {
   public void run(String[] args) {
     AbstractApplicationContext context;
 
-    context = new ClassPathXmlApplicationContext(configLocation);
+    context = new ClassPathXmlApplicationContext(defaultConfigLocation, configLocation);
 
     context.registerShutdownHook();
   }
